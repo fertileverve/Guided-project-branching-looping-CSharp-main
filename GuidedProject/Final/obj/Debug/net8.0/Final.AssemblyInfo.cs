16feb87b713f9a6f594b7acb223622214bdc2f7a
Joseph Parchem <fertileverve@gmail.com>
2025-09-29 14:32:08 -0500

    I have completed the guided exercise and am ready to check in my changes.

--- GuidedProject/Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
+++ GuidedProject/Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c00bacf2f06f12f45fbdd2e5c2c4f2189f65cc86")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
